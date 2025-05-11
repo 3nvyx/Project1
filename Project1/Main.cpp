dfdf385cc9bcfd1ebdf3e3b551a308fae456c404
3nvyx <125855120+3nvyx@users.noreply.github.com>
2025-05-11 00:19:19 -0700

    XS

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -11,6 +11,9 @@
     
     Workshop Hub
 */
+/*
+    Comprehensive test for WorkshopList, DataLoader, ParticipantList, and Participant
+*/
 
 #include "DataLoader.h"
 #include "WorkshopList.h"
@@ -20,39 +23,30 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-int main() 
-{
-    WorkshopList myWorkshopList;
+int main() {
+    // --- Test WorkshopList & DataLoader ---
+
+    WorkshopList workshopList;
     DataLoader loader;
 
-    ifstream infile("workshop_database.txt");
+    // 1. isEmpty on fresh list
+    cout << "workshopList.isEmpty()? "
+        << (workshopList.isEmpty() ? "Yes" : "No") << "\n";
 
+    // 2. Load from file
+    ifstream infile("/Users/envn0/Downloads/workshop_database.txt");
 
-    // Participants
-    ParticipantList list;
-    Participant p1(100, "Joe", "Doh");
-    Participant p2(120, "Santa", "Claus");
-    Participant p3(130, "Enigma", "Man");
-
-    list.addParticipant(p1);
-    list.addParticipant(p2);
-    list.addParticipant(p3);
-
-    cout << "\ntesting participants \n";
-    cout << "First name of p1: " << p1.getFirstName() << endl;
-    cout << "Last name of p1: " << p1.getLastName() << endl;
-    cout << "ID of p1: " << p1.getID() << endl;
-
-    cout << "First name of p2: " << p2.getFirstName() << endl;
-    cout << "Last name of p2: " << p2.getLastName() << endl;
-    cout << "ID of p2: " << p2.getID() << endl;
-
-    cout << "First name of p3: " << p3.getFirstName() << endl;
-    cout << "Last name of p3: " << p3.getLastName() << endl;
-    cout << "ID of p3: " << p3.getID() << endl;
+    // 2. Create Participants and add
+    Participant a(1, "Alice", "Anderson");
+    Participant b(2, "Bob", "Brown");
+    Participant c(3, "Cara", "Clark");
+    participantList.addParticipant(a);
+    participantList.addParticipant(b);
+    participantList.addParticipant(c);
 
     return 0;
 }
