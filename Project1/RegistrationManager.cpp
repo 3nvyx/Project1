22e300812eb70ef0e3a49a789d578a7de04ddc74
jtruong112 <145070446+jtruong112@users.noreply.github.com>
2025-05-18 17:42:41 -0700

    Update RegistrationManager.cpp

--- Project1/RegistrationManager.cpp
+++ Project1/RegistrationManager.cpp
@@ -11,3 +11,46 @@
 
 		Workshop Hub
 */
+
+#include "RegistrationManager.h"
+
+using namespace std
+
+void RegistrationManager::addOpenWorkshop(int workshopNo)
+{
+	openWorkshops.insert(workshopNo);
+	registration.insert(worshopNo, set<int> participantIDs {});
+}
+
+void RegistrationManager::registerParticipant(int workshopNo,
+                             int participantID)
+{
+	auto iter = find(workshopNo);
+	
+}
+
+void RegistrationManager::unregisterParticipant(int workshopNo,
+                               int participantID)
+{
+
+}
+
+void RegistrationManager::closeWorkshop(int workshopNo)
+{
+
+}
+
+void RegistrationManager::reopenWorkshop(int workshopNo)
+{
+
+}
+
+bool RegistrationManager::isOpen(int workshopNo)
+{
+
+}
+
+const std::set<int> &RegistrationManager::getOpenWorkshop()
+{
+
+}
